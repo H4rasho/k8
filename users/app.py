472ff608029ca835130ae9116256996cc1c1d6ed
h4rasho <h4rasho@gmail.com>
2023-04-28 11:22:59 -0400

    add db connection

--- users/app.py
+++ users/app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+import psycopg2
 app = Flask(__name__)
+conn = psycopg2.connect("dbname=zoo user=postgres password=gdk1JlTM host=pg_db")
+cur = conn.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS users (id serial PRIMARY KEY, name varchar);")
 
 @app.route('/')
-def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
+def getUsers():
+    cur.execute("SELECT * FROM users;")
+    # Return a list of users in JSON format
+    return str(cur.fetchall())
 
 
 if __name__ == "__main__":
